1d83a3bc6fb5b9c1a8f263add78df568f1c5f6f3
sumitsasidharan <s.sumit196@gmail.com>
2022-08-12 21:42:29 +0530

    add reverse string algorithm

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # AlgoCasts
 
 Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
+
+## IMP NOTES
+
+### Steps for Debugging
+
+1. To debug a file or run debugger, add a 'debugger' statement in your function.
+2. Call the function manually.
+3. At the terminal, run `node inspect filename.js`.
+4. To continue execution of the file, press 'c' then 'enter'.
+5. To laungh a 'repl' session, type 'repl' and <enter>. You can type variable names or code to debug.
+6. To exit the 'repl', press control + C. To exit debut, type '.exit'.
+
+## 1. Reverse a String
+
+### Algorithm
+
+      1. Create an empty string called 'reversed'
+      2. for each character in the provided string, Take the charcter and add it to the start of 'reversed'
+      3. Return the variable 'reversed'
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,16 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/* using inbuilt methods
+   reversed Array = arr.split('').reverse().join('')
+*/
+
+// My Solution
 function reverse(str) {
-   let reversedString = "";
+   let reversedString = '';
 
    for (let i = 0; i < str.length; i++) {
+      console.log(
+         `Iteration ${i}: reversedString = ${str[i]} + ${reversedString}`
+      );
+
       reversedString = str[i] + reversedString;
    }
 
    return reversedString;
 }
 
-console.log(reverse('sumit'));
+// Stephen's solution 1
+function reverseString(str) {
+   let reversed = '';
+
+   for (const char of str) {
+      reversed = char + reversed;
+   }
+
+   return reversed;
+}
+
+// Stephen's solution 2
+function reverseString2(str) {
+   return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+// console.log(reverseString2('sumit'));
 
 module.exports = reverse;
