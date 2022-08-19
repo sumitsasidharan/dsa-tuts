7775b2ac0231b3197303421861941ed7bcff5c4a
sumitsasidharan <s.sumit196@gmail.com>
2022-08-19 22:46:45 +0530

    updated maxchar

--- exercises/04_maxchar/index.js
+++ exercises/04_maxchar/index.js
@@ -5,6 +5,55 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// my solution
+function maxChar1(str) {
+   const chars = {};
+
+   for (let char of str) {
+      // chars[char] ? chars[char]++ : chars[char] = 1;
+      chars[char] = chars[char] + 1 || 1;
+   }
+
+   let max = 0;
+   let mxChar;
+   for (let char in chars) {
+      if (chars[char] > max) {
+         max = chars[char];
+         mxChar = char
+      }
+   }
+
+   return mxChar;
+}
+
+console.log({
+   testcase1: maxChar('sumit'),
+   testcase2: maxChar('abba'),
+   testcase3: maxChar('abcddeee'),
+});
+
+// Solution
+function maxChar(str) {
+   const charMap = {};
+   let max = 0;
+   let maxChar = "";
+
+   for (let char of str) {
+      if (charMap[char]) {
+         charMap[char]++;
+      } else {
+         charMap[char] = 1;
+      }
+   }
+
+   for (let char in charMap) {
+      if (charMap[char] > max) {
+         max = charMap[char];
+         maxChar = char;
+      }
+   }
+
+   return maxChar;
+}
 
 module.exports = maxChar;
