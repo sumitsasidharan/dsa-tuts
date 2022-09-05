026873cca696dca4ac567646da2c35f69923ff2f
sumitsasidharan <s.sumit196@gmail.com>
2022-09-05 22:57:41 +0530

    finished mergesort

--- exercises/28_sorting/index.js
+++ exercises/28_sorting/index.js
@@ -36,14 +36,32 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+   if (arr.length === 1) {
+      return arr;
+   }
 
+   const center = Math.floor(arr.length / 2);
+   const left = arr.slice(0, center);
+   const right = arr.slice(center);
+
+   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+   const results = [];
 
+   while (left.length && right.length) {
+      if (left[0] < right[0]) {
+         results.push(left.shift());
+      } else {
+         results.push(right.shift());
+      }
+   }
+
+   return [...results, ...left,  ...right];
 }
 
-console.log(bubbleSort([2, 77, 23, 3, 76, 22, 11, 5, 99]))
-console.log(selectionSort([2, 77, 23, 3, 76, 22, 11, 5, 99]));
+// console.log(bubbleSort([2, 77, 23, 3, 76, 22, 11, 5, 99]))
+// console.log(selectionSort([2, 77, 23, 3, 76, 22, 11, 5, 99]));
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
